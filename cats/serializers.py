c2b7a2a9764050a4cd6550d20f2ddda05d667a08
inkoff <kob87@list.ru>
2022-12-03 16:10:45 +0300

    fix
    Signed-off-by: inkoff <kob87@list.ru>

--- cats/serializers.py
+++ cats/serializers.py
@@ -6,4 +6,5 @@ from .models import Cat
 class CatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cat
-        fields = '__all__'
+        # fields = "__all__"
+        fields = ("name", "color", "birth_year")
--- cats/views.py
+++ cats/views.py
@@ -1,15 +1,16 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.request import Request
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-@api_view(['GET', 'POST'])
-def cat_list(request):
-    if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
+@api_view(["GET", "POST"])
+def cat_list(request: Request) -> Response:
+    if request.method == "POST":
+        serializer = CatSerializer(data=request.data, many=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
